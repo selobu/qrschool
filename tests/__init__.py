414a8e4613225a690c0a631fdd25957fc6c6d8d2
Sebastian Lopez Buritica <selobu@gmail.com>
2023-08-11 16:25:17 -0500

    Update __init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,8 @@
+from sys import path
+from pathlib import Path
+from os.path import abspath, join
 
+# Add current path to sys.path
+cp = Path(__file__).parent
+if abspath(cp) not in path:
+    path.append(abspath(cp))
