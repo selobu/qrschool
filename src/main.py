5b0d31460cd33ac98ef357b3d1866a0b232e866d
Sebastian Lopez Buritica <selobu@gmail.com>
2023-10-09 06:01:16 -0500

    build: :green_heart: reading configuration from local files

--- src/main.py
+++ src/main.py
@@ -2,9 +2,8 @@
 __version__ = "0.1.0"
 
 from app import create_app
-from app.config import PythonAnywhereConfig
 
-app = create_app(PythonAnywhereConfig)
+app = create_app()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8082, debug=False)
