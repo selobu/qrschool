5c5ef63733e1c97f42fdb4f9d320254f792aac71
Sebastian Lopez Buritica <selobu@gmail.com>
2023-10-08 20:37:55 -0500

    refactor: :green_heart: Setting default configuration to pythonanywhere

--- src/main.py
+++ src/main.py
@@ -2,9 +2,9 @@
 __version__ = "0.1.0"
 
 from app import create_app
-from app.config import ProductionConfig
+from app.config import PythonAnywhereConfig
 
-app = create_app(ProductionConfig)
+app = create_app(PythonAnywhereConfig)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8082, debug=True)
