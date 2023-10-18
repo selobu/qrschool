0699d5847f5dc2827e0344da0ff5180c0b61605d
Sebastian Lopez Buritica <selobu@gmail.com>
2023-10-18 16:05:07 -0500

    refactor: :recycle: separate asistencia models

---
+++ src/app/modules/Asistencia/view.py
@@ -0,0 +1,42 @@
+__all__ = [
+    "ns_asistencia",
+    "qr_register_list",
+    "usr",
+    "usr_list_paginated",
+    "asistencia",
+]
+from flask import current_app as app
+from app.apitools import createApiModel
+from flask_restx import fields
+
+api = app.api  # type: ignore
+
+ns_asistencia = api.namespace(
+    "asistencia", description="Registrar asistencia de usuarios"
+)
+
+usr = createApiModel(api, Tb.User, "Usuario", readonlyfields=["active"])  # type: ignore
+
+qr_register_list = api.model(
+    "QrRegisterList",
+    {
+        "qrs": fields.List(
+            fields.String(description="User QR code", required=True),
+            description="Listado de códigos Qr",
+        )
+    },
+)
+
+usr_list_paginated = api.model(
+    "UsersResList", {"usrs": fields.List(fields.Nested(usr))}
+)
+asistencia = api.model(
+    "asistencia",
+    {
+        "asistenciaid": fields.Integer(description="Asistencia id"),
+        "total": fields.Integer(
+            description="Cantidad total de personas en la asistencia"
+        ),
+        "timestamp": fields.DateTime(description="Fecha de registro"),
+    },
+)
